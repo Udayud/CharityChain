d43b300300bc5317f4b449718204ff275ce505cc
Uday <udaydeshmukh.ud@gmail.com>
2020-09-23 21:36:20 +0530

    initial Init

---
+++ contracts/Crowdfunding.sol
@@ -0,0 +1,285 @@
+// We will be using Solidity version 0.5.4
+pragma solidity 0.5.4;
+// Importing OpenZeppelin's SafeMath Implementation
+import 'https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol';
+
+
+contract Crowdfunding {
+    using SafeMath for uint256;
+
+    // List of existing projects
+    Project[] private projects;
+
+    // Event that will be emitted whenever a new project is started
+    event ProjectStarted(
+        address contractAddress,
+        address projectStarter,
+        string projectTitle,
+        string projectDesc,
+        uint256 deadline,
+        uint256 goalAmount
+    );
+
+    /** @dev Function to start a new project.
+      * @param title Title of the project to be created
+      * @param description Brief description about the project
+      * @param durationInDays Project deadline in days
+      * @param amountToRaise Project goal in wei
+      */
+    function startProject(
+        string calldata title,
+        string calldata description,
+        uint durationInDays,
+        uint amountToRaise
+    ) external {
+        uint raiseUntil = now.add(durationInDays.mul(1 days));
+        Project newProject = new Project(msg.sender, title, description, raiseUntil, amountToRaise);
+        projects.push(newProject);
+        emit ProjectStarted(
+            address(newProject),
+            msg.sender,
+            title,
+            description,
+            raiseUntil,
+            amountToRaise
+        );
+    }                                                                                                                                   
+
+    /** @dev Function to get all projects' contract addresses.
+      * @return A list of all projects' contract addreses
+      */
+    function returnAllProjects() external view returns(Project[] memory){
+        return projects;
+    }
+}
+
+
+contract Project {
+    using SafeMath for uint256;
+    
+    // Data structures
+    enum State {
+        Fundraising,
+        Expired,
+        Successful
+    }
+
+    // State variables
+    address payable public creator;
+    uint public amountGoal; // required to reach at least this much, else everyone gets refund
+    uint public completeAt;
+    uint256 public currentBalance;
+    uint public raiseBy;
+    string public title;
+    string public description;
+    State public state = State.Fundraising; // initialize on create
+    mapping (address => uint) public contributions;
+
+    // Event that will be emitted whenever funding will be received
+    event FundingReceived(address contributor, uint amount, uint currentTotal);
+    // Event that will be emitted whenever the project starter has received the funds
+    event CreatorPaid(address recipient);
+
+    // Modifier to check current state
+    modifier inState(State _state) {
+        require(state == _state);
+        _;
+    }
+
+    // Modifier to check if the function caller is the project creator
+    modifier isCreator() {
+        require(msg.sender == creator);
+        _;
+    }
+
+    constructor
+    (
+        address payable projectStarter,
+        string memory projectTitle,
+        string memory projectDesc,
+        uint fundRaisingDeadline,
+        uint goalAmount
+    ) public {
+        creator = projectStarter;
+        title = projectTitle;
+        description = projectDesc;
+        amountGoal = goalAmount;
+        raiseBy = fundRaisingDeadline;
+        currentBalance = 0;
+    }
+
+    /** @dev Function to fund a certain project.
+      */
+    function contribute() external inState(State.Fundraising) payable {
+        require(msg.sender != creator);
+        contributions[msg.sender] = contributions[msg.sender].add(msg.value);
+        currentBalance = currentBalance.add(msg.value);
+        emit FundingReceived(msg.sender, msg.value, currentBalance);
+        checkIfFundingCompleteOrExpired();
+    }
+
+    /** @dev Function to change the project state depending on conditions.
+      */
+    function checkIfFundingCompleteOrExpired() public {
+        if (currentBalance >= amountGoal) {
+            state = State.Successful;
+            payOut();
+        } else if (now > raiseBy)  {
+            state = State.Expired;
+        }
+        completeAt = now;
+    }
+
+    /** @dev Function to give the received funds to project starter.
+      */
+    function payOut() internal inState(State.Successful) returns (bool) {
+        uint256 totalRaised = currentBalance;
+        currentBalance = 0;
+
+        if (creator.send(totalRaised)) {
+            emit CreatorPaid(creator);
+            return true;
+        } else {
+            currentBalance = totalRaised;
+            state = State.Successful;
+        }
+
+        return false;
+    }
+
+    /** @dev Function to retrieve donated amount when a project expires.
+      */
+    function getRefund() public inState(State.Expired) returns (bool) {
+        require(contributions[msg.sender] > 0);
+
+        uint amountToRefund = contributions[msg.sender];
+        contributions[msg.sender] = 0;
+
+        if (!msg.sender.send(amountToRefund)) {
+            contributions[msg.sender] = amountToRefund;
+            return false;
+        } else {
+            currentBalance = currentBalance.sub(amountToRefund);
+        }
+
+        return true;
+    }
+
+    /** @dev Function to get specific information about the project.
+      * @return Returns all the project's details
+      */
+    function getDetails() public view returns 
+    (
+        address payable projectStarter,
+        string memory projectTitle,
+        string memory projectDesc,
+        uint256 deadline,
+        State currentState,
+        uint256 currentAmount,
+        uint256 goalAmount
+    ) {
+        projectStarter = creator;
+        projectTitle = title;
+        projectDesc = description;
+        deadline = raiseBy;
+        currentState = state;
+        currentAmount = currentBalance;
+        goalAmount = amountGoal;
+    }
+}
+
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+
+    function createCampaign(uint minimum) public {
+        address newCampaign = address(new Campaign(minimum, msg.sender));
+        deployedCampaigns.push(newCampaign);
+    }
+
+    function getDeployedCampaigns() public view returns(address[] memory) {
+        return deployedCampaigns;
+    }
+}
+
+contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address payable recipient;
+        bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
+    }
+
+    address public manager;
+    uint public minimumContribution;
+    mapping(address => bool) public approvers;
+    uint public approversCount;
+    Request[] public requests;
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    constructor(uint minimum, address creator) public {
+        manager = creator;
+        minimumContribution = minimum;
+    }
+
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+        approvers[msg.sender] = true;
+        approversCount++;
+    }
+
+    function createRequest(string memory description, uint value, address payable recipient) public restricted {
+        Request memory newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false,
+            approvalCount: 0
+        });
+        requests.push(newRequest);
+    }
+
+    function getRequest(uint index) public view returns(string memory, uint, address, bool, uint) {
+        Request memory request = requests[index];
+        return (request.description, request.value, request.recipient, request.complete, request.approvalCount);
+    }
+
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
+
+        require(request.approvalCount > (approversCount / 2));
+        require(!request.complete);
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
+
+    function getSummary() public view returns (uint, uint, uint, uint, address) {
+        return (
+            minimumContribution,
+            address(this).balance,
+            requests.length,
+            approversCount,
+            manager
+        );
+    }
+
+    function getRequestsCount() public view returns (uint) {
+        return requests.length;
+    }
+}
---
+++ contracts/crowdfundInstance.js
@@ -0,0 +1,89 @@
+/* eslint-disable */
+import web3 from './web3';
+
+const address = '0xe01228b65920f664b892e5ba64870ee592d889e6';
+const abi =
+	[
+		{
+			"constant": true,
+			"inputs": [],
+			"name": "returnAllProjects",
+			"outputs": [
+				{
+					"name": "",
+					"type": "address[]"
+				}
+			],
+			"payable": false,
+			"stateMutability": "view",
+			"type": "function"
+		},
+		{
+			"constant": false,
+			"inputs": [
+				{
+					"name": "title",
+					"type": "string"
+				},
+				{
+					"name": "description",
+					"type": "string"
+				},
+				{
+					"name": "durationInDays",
+					"type": "uint256"
+				},
+				{
+					"name": "amountToRaise",
+					"type": "uint256"
+				}
+			],
+			"name": "startProject",
+			"outputs": [],
+			"payable": false,
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"anonymous": false,
+			"inputs": [
+				{
+					"indexed": false,
+					"name": "contractAddress",
+					"type": "address"
+				},
+				{
+					"indexed": false,
+					"name": "projectStarter",
+					"type": "address"
+				},
+				{
+					"indexed": false,
+					"name": "projectTitle",
+					"type": "string"
+				},
+				{
+					"indexed": false,
+					"name": "projectDesc",
+					"type": "string"
+				},
+				{
+					"indexed": false,
+					"name": "deadline",
+					"type": "uint256"
+				},
+				{
+					"indexed": false,
+					"name": "goalAmount",
+					"type": "uint256"
+				}
+			],
+			"name": "ProjectStarted",
+			"type": "event"
+		}
+	];
+
+
+const instance = new web3.eth.Contract(abi, address);
+
+export default instance;
